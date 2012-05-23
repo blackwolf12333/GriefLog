3d40b3b9a5b5a8a5d5a98f9799cfe81e04703271
blackwolf12333 <blackwolf12333@gmail.com>
2012-05-23 18:27:57 +0300

    Update src/tk/blackwolf12333/grieflog/api/GriefLogAPI.java

--- src/tk/blackwolf12333/grieflog/api/GriefLogAPI.java
+++ src/tk/blackwolf12333/grieflog/api/GriefLogAPI.java
@@ -11,3 +11,5 @@ public class GriefLogAPI {
 
 	
 }
+// this class is not used, i think you guys just make your plugins and then just add my plugin
+// to your classpath, do make sure that you disable your plugin when grieflog isn't in the server your plugin runs on
