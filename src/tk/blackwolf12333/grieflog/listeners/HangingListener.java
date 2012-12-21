fd9d56a2c13a6d8e9c356961c6a9abc33440dbad
blackwolf12333 <blackwolf12333@gmail.com>
2012-12-21 14:24:24 +0100

    Started implementing logging of Hanging entities(paintings etc)

--- src/tk/blackwolf12333/grieflog/listeners/HangingListener.java
+++ src/tk/blackwolf12333/grieflog/listeners/HangingListener.java
@@ -7,8 +7,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.event.hanging.HangingPlaceEvent;
 
-import tk.blackwolf12333.grieflog.data.block.BlockBreakData;
 import tk.blackwolf12333.grieflog.data.block.BlockPlaceData;
+import tk.blackwolf12333.grieflog.data.hanging.HangingBreakData;
 import tk.blackwolf12333.grieflog.utils.logging.GriefLogger;
 
 public class HangingListener implements Listener {
@@ -22,9 +22,8 @@ public class HangingListener implements Listener {
 			String world = event.getEntity().getLocation().getWorld().getName();
 			String playerName = (event.getRemover() instanceof Player) ? ((Player) event.getRemover()).getName() : "Entity";
 			Integer gamemode = (event.getRemover() instanceof Player) ? ((Player) event.getRemover()).getGameMode().getValue() : 0;
-			String blockType = "Painting";
-			byte blockData = 0x0;
-			BlockBreakData data = new BlockBreakData(x, y, z, world, blockType, blockData, playerName, gamemode);
+			String hangingType = event.getEntity().getType().toString();
+			HangingBreakData data = new HangingBreakData(x, y, z, world, playerName, gamemode, hangingType);
 			new GriefLogger(data);
 		}
 	}
@@ -32,12 +31,12 @@ public class HangingListener implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onHangingPlace(HangingPlaceEvent event) {
 		if(!event.isCancelled()) {
-			int blockX = event.getBlock().getX();
-			int blockY = event.getBlock().getY();
-			int blockZ = event.getBlock().getZ();
-			String world = event.getBlock().getWorld().getName();
-			String blockType = event.getBlock().getType().toString();
-			byte blockData = event.getBlock().getData();
+			int blockX = event.getEntity().getLocation().getBlockX();
+			int blockY = event.getEntity().getLocation().getBlockY();
+			int blockZ = event.getEntity().getLocation().getBlockZ();
+			String world = event.getEntity().getWorld().getName();
+			String blockType = event.getEntity().getType().toString();
+			byte blockData = 0x0;
 			String playerName = event.getPlayer().getName();
 			Integer gamemode = event.getPlayer().getGameMode().getValue();
 			BlockPlaceData data = new BlockPlaceData(blockX, blockY, blockZ, blockType, blockData, world, playerName, gamemode);
