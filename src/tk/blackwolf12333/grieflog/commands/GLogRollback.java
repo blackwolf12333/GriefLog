4f6989f3b59850d46c4084ee6ce749715dc07d10
blackwolf12333 <blackwolf12333@gmail.com>
2012-12-29 16:59:21 +0100

    Cleaned this a bit up, now having separated methods for stuff.

--- src/tk/blackwolf12333/grieflog/commands/GLogRollback.java
+++ src/tk/blackwolf12333/grieflog/commands/GLogRollback.java
@@ -6,10 +6,10 @@ import org.bukkit.ChatColor;
 
 import tk.blackwolf12333.grieflog.GriefLog;
 import tk.blackwolf12333.grieflog.PlayerSession;
-import tk.blackwolf12333.grieflog.callback.RegionRollbackCallback;
 import tk.blackwolf12333.grieflog.callback.RollbackCallback;
-import tk.blackwolf12333.grieflog.callback.WorldEditFilterCallback;
 import tk.blackwolf12333.grieflog.conversations.RollbackConversation;
+import tk.blackwolf12333.grieflog.utils.filters.BlockFilter;
+import tk.blackwolf12333.grieflog.utils.filters.WorldEditFilter;
 import tk.blackwolf12333.grieflog.utils.searching.ArgumentParser;
 import tk.blackwolf12333.grieflog.utils.searching.SearchTask;
 
@@ -25,51 +25,29 @@ public class GLogRollback {
 	
 	public boolean onCommand(PlayerSession player, String[] args) {
 		if (player.hasPermission("grieflog.rollback")) {
-			if(args.length == 1) {
-				if(player.isDoingRollback()) {
-					player.getPlayer().sendMessage(ChatColor.YELLOW + "[GriefLog] You are already doing a rollback, you can't have multiple rollbacks at the time.");
-					return true;
-				} else { 
-					new RollbackConversation(plugin, player);
-				}
+			if(player.isDoingRollback()) {
+				player.getPlayer().sendMessage(ChatColor.YELLOW + "[GriefLog] You are already doing a rollback, you can't have multiple rollbacks at the time.");
 				return true;
+			}
+			if(args.length == 1) {
+				return useConversations(player);
 			} else if(args[1].equalsIgnoreCase("we")) {
-				if(args.length == 2) {
-					if(player.isDoingRollback()) {
-						player.getPlayer().sendMessage(ChatColor.YELLOW + "[GriefLog] You are already doing a rollback, you can't have multiple rollbacks at the time.");
-						return true;
-					} else { 
-						new RollbackConversation(plugin, player);
-					}
-					return true;
-				}
 				if(player.getPlayer() != null) {
-					ArrayList<String> arguments = new ArrayList<String>();
-					
-					if(player.isDoingRollback()) {
-						player.getPlayer().sendMessage(ChatColor.YELLOW + "[GriefLog] You are already doing a rollback, you can't have multiple rollbacks at the time.");
-						return true;
+					if(args.length == 2) {
+						return useConversations(player);
 					} else {
-						for(int i = 0; i < args.length; i++) {
-							if(i >= 2) {
-								arguments.add(args[i]);
+						ArgumentParser parser = parseWorldEditArguments(args);
+						
+						if(checkParserErrors(parser, player)) {
+							if(parser.blockFilter != null) {
+								GriefLog.debug("block filter");
+								addParserResultsToUndoConfig(parser, true);
+								new SearchTask(player, new RollbackCallback(player), parser, new BlockFilter(player, parser.blockFilter));
+							} else {
+								addParserResultsToUndoConfig(parser, true);
+								new SearchTask(player, new RollbackCallback(player), parser, new WorldEditFilter(player));
 							}
 						}
-						
-						ArgumentParser parser = new ArgumentParser(arguments);
-						
-						if(parser.argsNullError) {
-							player.print(ChatColor.DARK_RED + "Sorry, an error occured. Please check if you formatted the arguments right.");
-							return true;
-						} else if(parser.eventNullError) { 
-							player.print(ChatColor.DARK_RED + "You misspelled the event, try again.");
-							return true;
-						} else {
-							String parsedArgs = "we:" + parser.event + ":" + parser.player + ":" + parser.world;
-							GriefLog.undoConfig.add(parsedArgs);
-							new SearchTask(player, new WorldEditFilterCallback(player, new RegionRollbackCallback(player)), parser);
-						}
-						
 						return true;
 					}
 				} else {
@@ -77,28 +55,61 @@ public class GLogRollback {
 					return true;
 				}
 			} else {
-				if(player.isDoingRollback()) {
-					player.print(ChatColor.YELLOW + "[GriefLog] You are already doing a rollback, you can't have multiple rollbacks at the time.");
-					return true;
-				} else {
-					ArgumentParser parser = new ArgumentParser(args);
-					if(parser.argsNullError) {
-						player.print(ChatColor.DARK_RED + "Sorry, an error occured. Please check if you formatted the arguments right.");
-					} else if(parser.eventNullError) { 
-						player.print(ChatColor.DARK_RED + "You misspelled the event, try again.");
+				ArgumentParser parser = new ArgumentParser(args);
+				if(checkParserErrors(parser, player)) {
+					if(parser.blockFilter != null) {
+						GriefLog.debug("block filter");
+						addParserResultsToUndoConfig(parser, false);
+						new SearchTask(player, new RollbackCallback(player), parser, new BlockFilter(player, parser.blockFilter));
 					} else {
-						String parsedArgs = parser.event + ":" + parser.player + ":" + parser.world;
-						GriefLog.undoConfig.add(parsedArgs);
+						addParserResultsToUndoConfig(parser, false);
 						new SearchTask(player, new RollbackCallback(player), parser);
 					}
-					
-					return true;
 				}
+				
+				return true;
 			}
 		} else {
 			player.print(noPermsMsg);
 			return true;
 		}
 	}
+	
+	private boolean useConversations(PlayerSession player) {
+		new RollbackConversation(plugin, player);
+		return true;
+	}
+	
+	private ArgumentParser parseWorldEditArguments(String[] args) {
+		ArrayList<String> arguments = new ArrayList<String>();
 		
+		for(int i = 0; i < args.length; i++) {
+			if(i >= 2) {
+				arguments.add(args[i]);
+			}
+		}
+		
+		return new ArgumentParser(arguments);
+	}
+	
+	private boolean checkParserErrors(ArgumentParser parser, PlayerSession player) {
+		if(parser.argsNullError) {
+			player.print(ChatColor.DARK_RED + "Sorry, an error occured. Please check if you formatted the arguments right.");
+			return false;
+		} else if(parser.eventNullError) { 
+			player.print(ChatColor.DARK_RED + "You misspelled the event, try again.");
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	private void addParserResultsToUndoConfig(ArgumentParser parser, boolean we) {
+		String parsedArgs;
+		if(we)
+			parsedArgs = "we:" + parser.event + ":" + parser.player + ":" + parser.world;
+		else
+			parsedArgs = parser.event + ":" + parser.player + ":" + parser.world;
+		GriefLog.undoConfig.add(parsedArgs);
+	}
 }
