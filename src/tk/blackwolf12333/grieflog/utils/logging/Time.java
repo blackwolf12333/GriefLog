1e9849ef59bf15f0837c466772b7fa4574e247a5
Peter Maatman <blackwolf12333@gmail.com>
2013-07-03 16:24:16 +0200

    Adding time argument to search

--- src/tk/blackwolf12333/grieflog/utils/logging/Time.java
+++ src/tk/blackwolf12333/grieflog/utils/logging/Time.java
@@ -2,6 +2,7 @@ package tk.blackwolf12333.grieflog.utils.logging;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import tk.blackwolf12333.grieflog.GriefLog;
@@ -15,7 +16,7 @@ public class Time {
 		return df.format(new Date());
 	}
 
-	public static Date getTimeStamp(String strDate) {
+	public static Date getDate(String strDate) {
 		Date date = null;
 		try {
 			DateFormat format = new SimpleDateFormat(DATE_FORMAT);
@@ -24,5 +25,61 @@ public class Time {
 			GriefLog.debug("Something went wrong with the date format parsing, tell this to blackwolf12333 please.");
 		}
 		return date;
+	}
+	
+	public static String getTimeFrom(String time) {
+		Calendar calendar = Calendar.getInstance();
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		int hour = calendar.get(Calendar.HOUR_OF_DAY);
+		int minute = calendar.get(Calendar.MINUTE);
+		int second = calendar.get(Calendar.SECOND);
+		
+		int index = 0;
+		int begin = 0;
+		for(int i = 0; i < time.length(); i++) {
+			if(isInt(time.charAt(index))) {
+				index++;
+			} else {
+				switch(time.charAt(i)) {
+				case 'd':
+					day -= Integer.parseInt(time.substring(begin, index));
+					begin+=index+1;
+					break;
+				case 'h':
+					hour -= Integer.parseInt(time.substring(begin, index));
+					begin+=index+1;
+					break;
+				case 'm':
+					minute -= Integer.parseInt(time.substring(begin, index));
+					begin+=index+1;
+					break;
+				case 's':
+					second -= Integer.parseInt(time.substring(begin, index));
+					begin+=index+1;
+					break;
+				}
+				index+=1;
+			}
+		}
+		
+		calendar.set(year, month, day, hour, minute, second);
+		DateFormat df = new SimpleDateFormat(DATE_FORMAT);
+		GriefLog.log.info("parsed time: " + df.format(calendar.getTime()));
+		GriefLog.log.info("current time: " + df.format(new Date()));
+		return df.format(calendar.getTime());
+	}
+	
+	private static boolean isInt(String s) {
+		try {
+			Integer.parseInt(s);
+			return true;
+		} catch(NumberFormatException e) {}
+		return false;
+	}
+	
+	private static boolean isInt(char c) {
+		return isInt(String.valueOf(c));
 	}
 }
